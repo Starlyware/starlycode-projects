bda05669bffb8e254d6eac38b458eabee2b2e84a
Gianluca Grasso <gianflauschig@gmail.com>
2022-09-22 15:17:42 +0200

    Changed 'Password Generator/index.php'

--- Password Generator/index.php
+++ Password Generator/index.php
@@ -1,17 +1,17 @@
 <?php
 function rPass($length = 6) {
 
-  $letters = explode(" ",
+  $char = explode(" ",
   "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z "
   ."a b c d e f g h i j k l m n o p q r s t u v w x y z "
-  ."0 1 2 3 4 5 6 7 8 9"
-  ."! £ $ % & / = @ +");
+  ."0 1 2 3 4 5 6 7 8 9 "
+  ."! £ $ % & / = @ + ");
   $pass = "";
 
   for($i=0;$i<$length;$i++) {
   srand((double)microtime()*8622342);
-  $foo = rand(0, 69);
-    $pass = $pass.$letters[$foo];
+  $foo = rand(0, 70);
+    $pass = $pass.$char[$foo];
   }
 
   return $pass;
